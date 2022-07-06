bda186903019afed05559e2b5e2ec7fdd7b80af1
Cynite <105969534+CyniteOfficial@users.noreply.github.com>
2022-07-06 16:11:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -29,12 +29,12 @@ bot = Client('Doodstream bot',
 async def start(bot, message):
     await message.reply(
         f"**Hi, {message.chat.first_name} !!**\n\n"
-        "**I am your Personal MDisk Bot 🤗, Made by @GreyMatter_bots💞 Send me a MDisk Post to see the Magic 😅**")
+        "**I am your Personal MDisk Link Converter Bot 🤗, Made by @TechnicalCynite💞 Send me a MDisk Post to see the Magic 😅**")
     
 @bot.on_message(filters.text & filters.private)
 async def Doodstream_uploader(bot, message):
     new_string = str(message.text)
-    conv = await message.reply("Ruko jara Sabar kro ✋")
+    conv = await message.reply("Wait Bnaraha Hu Tab Tak @TechnicalCynite Check Karo✋")
     dele = conv["message_id"]
     try:
         Doodstream_link = await multi_Doodstream_up(new_string)
@@ -47,7 +47,7 @@ async def Doodstream_uploader(bot, message):
 @bot.on_message(filters.photo & filters.private)
 async def Doodstream_uploader(bot, message):
     new_string = str(message.caption)
-    conv = await message.reply("Ruko jara Sabar kro ✋")
+    conv = await message.reply("Wait Bnaraha Hu Tab Tak @TechnicalCynite Check Karo✋")
     dele = conv["message_id"]
     try:
         Doodstream_link = await multi_Doodstream_up(new_string)
